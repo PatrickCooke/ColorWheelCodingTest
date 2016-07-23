19a200bf8df2ae9f7f06802be7d6ea1a54a1bb73
PatrickCooke <cookepa1@gmail.com>
2016-07-22 19:20:26 -0700

    Initial Commit

---
+++ ObjCCodeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCCodeChallenge
+//
+//  Created by Patrick Cooke on 7/22/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCCodeChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCCodeChallenge
+//
+//  Created by Patrick Cooke on 7/22/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
